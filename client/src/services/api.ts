03950e15e99fab474b4460603741ae44ca4a5ee1
Shahil Jha <shahilkumar0511@gmail.com>
2025-07-24 15:01:19 +0530

    changed for local server starts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
-  // baseURL: "https://execflow.onrender.com/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://execflow.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
